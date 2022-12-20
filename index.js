94a77a245b88656c20c105c94453b62153f2e13f
Pooja-Lohar25 <poojalohar789@gmail.com>
2022-12-21 03:11:19 +0530

    updated

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const heyy = require("./sec")
-
-console.log(heyy("hii"))
+const os = require('os')
+exp = {
+    nme: "pooja",
+    work : "swe"
+}
+//console.log(heyy("hii"))
+module.exports = exp
+//console.log(exports,require,__filename,__dirname)
+//os module methods
+//console.log(os.freemem()) //returns free memory available
+//console.log(os.uptime())
+console.log(os.type())
