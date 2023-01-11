a7b1124edad34266783eba0a068b8b9dc084d473
Pooja-Lohar25 <poojalohar789@gmail.com>
2023-01-11 12:20:51 +0530

    read write streams

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
 //following code will be initiated on the start of the script automatically
 //refer to start attribute of scripts in package.json
 //console.log("welcome to new landing page")
-
-//creating a big file
-// const fs = require('fs')
-// for(let i=0 ;i<10000;i++){
-//     fs.writeFileSync('./big.txt',`hello world ${i}\n`,{flag:'a'})
-// }
-
-const {createReadStream} = require('fs')
-const stream = createReadStream('./big.txt',{encoding:'utf8'})
-
-//reading the data
-stream.on('data',(result) =>{
-    console.log(result);
-})
-
-//reading the error
-stream.on('error',(err) =>{
-    console.log(err);
-})
-
--- events/streams.js
+++ events/streams.js
@@ -8,3 +8,42 @@ const stream = createReadStream('./big.txt')
 stream.on('data',(result) =>{
     console.log(result);
 })
+
+//creating a big file
+// const fs = require('fs')
+// for(let i=0 ;i<10000;i++){
+//     fs.writeFileSync('./big.txt',`hello world ${i}\n`,{flag:'a'})
+// }
+
+//reading the data
+stream.on('data',(result) =>{
+    console.log(result);
+})
+
+//reading the error
+stream.on('error',(err) =>{
+    console.log(err);
+})
+
+const fs = require('fs')
+const http = require('http')
+
+
+/**
+ * using readStreams you can send data in chunks over the network which is more efficient 
+ * and faster way of sending data from server 
+ */
+
+const server = http.createServer((req,res)=>{
+    const text = fs.createReadStream('./big.txt',{encoding:'utf8'})
+    text.on('open',()=>{
+        text.pipe(res)
+
+    })
+    text.on('error',(err)=>{
+        res.end(err)
+    })
+});
+
+
+server.listen(5000)
